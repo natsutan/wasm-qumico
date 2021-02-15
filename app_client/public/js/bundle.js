440c4741f7380a28b6547f54a6831979cbb656b5
natsutan <YRW03704@nifty.com>
2021-02-16 00:23:54 +0900

    work

---
+++ app_client/public/js/bundle.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+eval("\r\n\n\n//# sourceURL=webpack://wasm-automl/./src/js/app.js?");
+/******/ })()
+;
