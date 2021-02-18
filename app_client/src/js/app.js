f566af0266ec607824c5af3eb6ca730d196405a9
natsutan <YRW03704@nifty.com>
2021-02-19 00:29:18 +0900

    カメラが動く用になった

--- app_client/src/js/app.js
+++ app_client/src/js/app.js
@@ -1,1 +1,34 @@
 
+window.onload = () => {
+    const video  = document.querySelector("#camera");
+    const canvas = document.querySelector("#picture");
+  
+    /** カメラ設定 */
+    const constraints = {
+      audio: false,
+      video: {
+        width: 224,
+        height: 224,
+        facingMode: "user"   // フロントカメラを利用する
+      }
+    };
+  
+    /**
+     * カメラを<video>と同期
+     */
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then( (stream) => {
+      video.srcObject = stream;
+      video.onloadedmetadata = (e) => {
+        video.play();
+      };
+    })
+    .catch( (err) => {
+      console.log(err.name + ": " + err.message);
+    });
+
+    
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+   
+
+  };
