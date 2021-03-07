48931d9f8c14a794654291c6f71c83b9233d4cb8
natsutan <YRW03704@nifty.com>
2021-03-07 16:14:19 +0900

    犬が表示できるようになった

--- app_client/src/js/app.js
+++ app_client/src/js/app.js
@@ -1,11 +1,18 @@
 
 window.onload = () => {
     const video  = document.querySelector("#camera");
-    const canvas = document.querySelector("#picture");
-  
+    const canvas = document.querySelector("#picture").getContext("2d");
+    const canvas_ret = document.querySelector("#result").getContext("2d");
+    var img_dog = new Image();
+    var img_cat = new Image();
+
+    img_dog.src = "dog.png"
+    img_cat.src = "cat.png"
+
+
     /** カメラ設定 */
     const constraints = {
-      audio: false,
+      audio: false, 
       video: {
         width: 224,
         height: 224,
@@ -26,9 +33,15 @@ window.onload = () => {
     .catch( (err) => {
       console.log(err.name + ": " + err.message);
     });
+    canvas.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-   
+    console.log(typeof(canvas_ret));
 
+    img_dog.onload = function() {
+      canvas_ret.drawImage(img_dog, 0, 0);
+      console.log("load img");
+      console.log(typeof(img_dog));
+      }
+
+    console.log("end of on load");
   };
